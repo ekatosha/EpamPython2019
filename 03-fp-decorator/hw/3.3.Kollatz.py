54818bf058da7c269c010b8dea24ad84e76b8c6c
ekatosha <44431149+ekatosha@users.noreply.github.com>
2019-11-28 00:21:38 +0300

    Add files via upload

---
+++ 03-fp-decorator/hw/3.3.Kollatz.py
@@ -0,0 +1,59 @@
+'''Гипотеза Коллатца
+Гипотеза может быть кратко выражена следующим образом:
+
+берём любое натуральное число n, если оно чётное, то делим его на 2
+если нечётное, то умножаем на 3 и прибавляем 1 (получаем 3n + 1)
+над полученным числом выполняем те же самые действия, и так далее
+Гипотеза Коллатца заключается в том, что какое бы начальное число n мы ни взяли, рано или поздно мы получим единицу.
+
+Пример
+Для числа 12:
+
+12 6 3 10 5 16 8 4 2 1
+
+Всего получаем 9 шагов.
+
+Задача
+Вычислить число шагов для числа n, согласно гипотезе Коллатца необходимых для достижения этим числом единицы, используя функциональный стиль. По возможности не забыть проверить пограничные условия и входные данные.
+
+def collatz_steps(n):
+ ...
+
+ assert collatz_steps(16) == 4
+ assert collatz_steps(12) == 9
+ assert collatz_steps(1000000) == 152 '''
+
+
+def even(num):
+    return int(num/2)
+
+
+def odd(num):
+    return int(num*3+1)
+
+
+def even_odd(num_d):
+    if num_d['number']%2 == 0:
+        return {'number': even(num_d['number']),
+            'count': num_d['count'] + 1}
+    return {'number': odd(num_d['number']),
+            'count': num_d['count'] + 1}
+
+
+def make_d(num):
+    return {'number': num, 'count': 0}
+
+
+def step(state):
+    if state['number'] == 1:
+        return state['count']
+    return step(even_odd(state))
+
+
+def collatz_steps(n):
+      return step(make_d(n))
+
+
+assert collatz_steps(16) == 4
+assert collatz_steps(12) == 9
+assert collatz_steps(1000000) == 152
