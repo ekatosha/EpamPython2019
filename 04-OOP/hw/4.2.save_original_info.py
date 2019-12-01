4f1b8307567d460934beab0be00283edf85e36c2
ekatosha <44431149+ekatosha@users.noreply.github.com>
2019-12-01 18:23:37 +0300

    Add files via upload

---
+++ 04-OOP/hw/4.2.save_original_info.py
@@ -0,0 +1,32 @@
+import functools
+
+
+def print_result(func):
+    @new_decorator(func)
+    def wrapper(*args, **kwargs):
+        """Function-wrapper which print result of an original function"""
+        result = func(*args, **kwargs)
+        print(result)
+        return result, args
+    return wrapper
+
+
+@print_result
+def custom_sum(*args):
+    """This function can sum any objects which have __add___"""
+    return functools.reduce(lambda x, y: x + y, args)
+
+
+def new_decorator(f1):
+    def wrapper2(wrapper_func, wraped=f1):
+        name = getattr(wraped, '__name__')
+        doc = getattr(wraped, '__doc__')
+        original_func = wraped
+
+        def wrapper3(*args, **kwargs):
+            return wraped(*args, **kwargs)
+        setattr(wrapper3, '__name__', name)
+        setattr(wrapper3, '__doc__', doc)
+        setattr(wrapper3, '__original_func', original_func)
+        return wrapper3
+    return wrapper2
