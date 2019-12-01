8788aec91251a731bd912e4a923e0e146f61fc4d
ekatosha <44431149+ekatosha@users.noreply.github.com>
2019-12-01 18:23:07 +0300

    Add files via upload

--- 04-OOP/hw/save_original_info.py
+++ 04-OOP/hw/save_original_info.py
@@ -1,13 +1,28 @@
+"""
+Написать декоратор который позволит сохранять информацию из
+исходной функции (__name__ and __doc__), а так же сохранит саму
+исходную функцию в атрибуте __original_func
+print_result изменять нельзя, за исключением добавления вашего
+декоратора на строку отведенную под него - замените комментарий
+До применения вашего декоратор будет вызываться AttributeError при custom_sum.__original_func
+Это корректное поведение
+После применения там должна быть исходная функция
+Ожидаемый результат:
+print(custom_sum.__doc__)  # 'This function can sum any objects which have __add___'
+print(custom_sum.__name__)  # 'custom_sum'
+print(custom_sum.__original_func)  # <function custom_sum at <some_id>>
+"""
+
 import functools
 
 
 def print_result(func):
-    @new_decorator(func)
+    # Place for new decorator
     def wrapper(*args, **kwargs):
         """Function-wrapper which print result of an original function"""
         result = func(*args, **kwargs)
         print(result)
-        return result, args
+        return result
     return wrapper
 
 
@@ -17,16 +32,13 @@ def custom_sum(*args):
     return functools.reduce(lambda x, y: x + y, args)
 
 
-def new_decorator(f1):
-    def wrapper2(wrapper_func, wraped=f1):
-        name = getattr(wraped, '__name__')
-        doc = getattr(wraped, '__doc__')
-        original_func = wraped
+if __name__ == '__main__':
+    custom_sum([1, 2, 3], [4, 5])
+    custom_sum(1, 2, 3, 4)
 
-        def wrapper3(*args, **kwargs):
-            return wraped(*args, **kwargs)
-        setattr(wrapper3, '__name__', name)
-        setattr(wrapper3, '__doc__', doc)
-        setattr(wrapper3, '__original_func', original_func)
-        return wrapper3
-    return wrapper2
+    print(custom_sum.__doc__)
+    print(custom_sum.__name__)
+    without_print = custom_sum.__original_func
+
+    # the result returns without printing
+    without_print(1, 2, 3, 4)
