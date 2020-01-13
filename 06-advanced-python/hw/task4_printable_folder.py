e22661d1956305be52d7cf68cd93f7390717dec9
ekatosha <44431149+ekatosha@users.noreply.github.com>
2020-01-13 22:55:19 +0300

    Update task4_printable_folder.py

--- 06-advanced-python/hw/task4_printable_folder.py
+++ 06-advanced-python/hw/task4_printable_folder.py
@@ -3,20 +3,21 @@ import os
 
 class PrintableFolder:
     def __init__(self, name, content=None):
-        self.name = name
+        index = name.rfind("\\")
+        self.name = name[index+1:]
         self.content = []
         self._print = []
-        index = name.rfind("\\")
         for root, dirs, files in os.walk(name, topdown=False):
             for file_name in files:
                 self.content.append(file_name)
                 self._print.append('{}\{}'.format(root[index:], file_name))
-            for file_name in dirs:
-                self.content.append(file_name)
-        self.name = name[index+1:]
+            for dir_name in dirs:
+                self.content.append(dir_name)
+                self._print.append('{}\{}'.format(root[index:], dir_name))
+        self._print.append(name[index:])
 
     def __str__(self):
-        return "\n".join(self._print)
+        return("\n".join(self._print))
 
     def __iter__(self):
         return iter(self.content)
@@ -24,7 +25,8 @@ class PrintableFolder:
 
 class PrintableFile:
     def __init__(self, name):
-        self.name = name
+        index = name.rfind("\\")
+        self.name = name[index+1:]
 
     def __str__(self):
         return self.name  # print out the file name
